455f2f798221af83279d42ee42187197009b3cea
danit-x <danithpasindu7777@gmail.com>
2026-03-07 00:24:26 +0530

    refactor: clean default template and add base layout

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-black text-white">
+    <h1 class="text-4xl font-bold text-center mt-20">
+      VYBE
+    </h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
