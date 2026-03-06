c4eeec183efe55c2b28a981aca55887813b2eebc
danit-x <danithpasindu7777@gmail.com>
2026-03-07 00:47:12 +0530

    feat(api): implement API service layer and fetch products from DummyJSON

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,5 +1,28 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue"
+import { getProducts } from "../services/productService"
+import type { Product } from "../types/product"
+
+const products = ref<Product[]>([])
+
+onMounted(async () => {
+  const data = await getProducts()
+  products.value = data.products
+})
+</script>
+
 <template>
   <div class="p-10">
-    <h1 class="text-3xl font-bold">Products</h1>
+    <h1 class="text-3xl font-bold mb-6">Products</h1>
+
+    <ul class="space-y-2">
+      <li
+        v-for="product in products"
+        :key="product.id"
+        class="border-b border-zinc-700 py-2"
+      >
+        {{ product.title }}
+      </li>
+    </ul>
   </div>
 </template>
